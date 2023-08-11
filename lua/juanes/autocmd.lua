13735c4103aee768eb46ce9ca1e03daaa3ec8d9c
OBLAK SAS <oblaksas@WLAPCOING72.local>
2023-08-11 09:36:00 -0500

    Configured python AUTOCMD to save the file before executing it

--- lua/juanes/autocmd.lua
+++ lua/juanes/autocmd.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function()
-    vim.keymap.set('n', '<leader>k', ':! python3 %<CR>')
+    vim.keymap.set('n', '<leader>k', ':w <CR> :! python3 %<CR>')
   end
 })
